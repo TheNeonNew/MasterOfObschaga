55f809c69b3f08dde80ae4c72cb98efe1459ea2d
Vlad Skarnovich <skarnovichvm@mer.ci.nsu.ru>
2024-10-30 15:31:36 +0700

    Add files via upload

---
+++ MasterOfObschaga/BaseCharacter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterOfObschaga
+{
+    abstract class BaseCharacter
+    {
+        protected Point location;
+        protected int hp, level;
+
+
+        internal BaseCharacter() { 
+            location = new Point(0, 0);
+            level = 1;
+        }
+
+
+
+    }
+}
---
+++ MasterOfObschaga/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterOfObschaga
+{
+    public partial class MainWindow : Window
+    {
+        private double playerX = 180;
+        private double playerY = 180;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdatePlayerPosition();
+        }
+
+        private void ExitGame(object sender, RoutedEventArgs e) { System.Environment.Exit(1); }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.W:
+                    if (playerY > 0) playerY -= 10;
+                    break;
+                case Key.S:
+                    if (playerY < GameCanvas.ActualHeight - 20) playerY += 10;
+                    break;
+                case Key.A:
+                    if (playerX > 0) playerX -= 10;
+                    break;
+                case Key.D:
+                    if (playerX < GameCanvas.ActualWidth - 20) playerX += 10;
+                    break;
+            }
+            UpdatePlayerPosition();
+        }
+
+        private void UpdatePlayerPosition()
+        {
+            Canvas.SetLeft(PlayerUnit, playerX);
+            Canvas.SetTop(PlayerUnit, playerY);
+        }
+    }
+}
